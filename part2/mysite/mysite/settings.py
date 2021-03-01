a59d6613ddb1889a0e3f0da4d5b703c8c31b2bcd
Dante Del Favero <dante.delfavero@gmail.com>
2021-03-01 16:16:58 +0100

    fixing SECRET_KEY app conf that bother me on public repos

--- part2/mysite/mysite/settings.py
+++ part2/mysite/mysite/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/3.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.1/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'z6b+uggsou(k0)1hpv$@2@qcmzq65@sfoxj2$5(^_p^7%62^f1'
+SECRET_KEY = os.environ['SECRET_KEY']
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
