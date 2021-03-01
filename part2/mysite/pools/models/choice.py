0ad979b2c261ee63f9a8b548c36fb0572bb46069
Dante Del Favero <dante.delfavero@gmail.com>
2021-03-01 17:29:54 +0100

    Adding models and migration

---
+++ part2/mysite/pools/models/choice.py
@@ -0,0 +1,7 @@
+# from django.db import models
+# from pools.models.question import Question
+
+# class Choice(models.Model):
+#     question = models.ForeignKey(Question, on_delete=models.CASCADE)
+#     choice_text = models.CharField(max_length=200)
+#     votes = models.IntegerField(default = 0)
