72e7b289a0a57198c908c2a7655e4ff959be80fe
Dante Del Favero <dante.delfavero@gmail.com>
2021-03-02 08:51:58 +0100

    Adding some view to site 3

--- part3/mysite/pools/views.py
+++ part3/mysite/pools/views.py
@@ -6,3 +6,15 @@ from django.http import HttpResponse
 
 def index(request):
     return HttpResponse("Hi there, this is my first <b>view</b>")
+
+def detail(request, question_id):
+    return HttpResponse('Question %s' % question_id)
+
+    
+def results(request, question_id):
+    response = "You are looking at the result of question %s."
+    return HttpResponse(response % question_id)
+
+    
+def vote(request, question_id):
+    return HttpResponse('Voting for %s' % question_id)
