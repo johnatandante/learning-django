950a0be8d9ea58cea30a4358ac330bcf5a708545
Dante Del Favero <dante.delfavero@gmail.com>
2021-03-02 09:02:24 +0100

    Updating the main view

--- part3/mysite/pools/views.py
+++ part3/mysite/pools/views.py
@@ -3,9 +3,16 @@ from django.shortcuts import render
 # Create your views here.
 
 from django.http import HttpResponse
+from .models import Question
+
+# old stuff
+# def index(request):
+#     return HttpResponse("Hi there, this is my first <b>view</b>")
 
 def index(request):
-    return HttpResponse("Hi there, this is my first <b>view</b>")
+    latest_question_list = Question.objects.order_by('-pub_date')[:5]
+    output = '<br /> '.join([q.question_text for q in latest_question_list])
+    return HttpResponse(output)
 
 def detail(request, question_id):
     return HttpResponse('Question %s' % question_id)
